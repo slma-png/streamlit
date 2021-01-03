c8d54da2c2b7a2b1e5f5c681e8793060cbfc7351
Anubhav Sinha <47371738+Anubhav1107@users.noreply.github.com>
2021-01-03 22:14:50 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     
 
         ax.set_axis_off()
-        st.image(cv2.cvtcolor(np.float32(sample)))
+        st.image(cv2.cvtcolor(np.array(sample)))
         st.write("# Results")
         st.dataframe(pd.DataFrame(results))
     else:
