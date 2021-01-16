1c46cc47d073ad9f87edd12516db5da76c36740c
Anubhav Sinha <47371738+Anubhav1107@users.noreply.github.com>
2021-01-16 16:12:34 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -52,11 +52,12 @@ def get_test_transform():
 def collate_fn(batch):
     return tuple(zip(*batch))
 
-@st.cache
+
 def download1(url1):
     url = url1
     filename = url.split('/')[-1]
     urllib.request.urlretrieve(url, filename)
+    
 download1("https://github.com/Anubhav1107/streamlit/releases/download/fasterrcnn.pth/fasterrcnn.pth")
 
 
